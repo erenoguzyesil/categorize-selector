a022177d9d5114ff8a88fd9312ed2f601488d6dc
erenoguzyesil <jfv.eoy@gmail.com>
2024-07-01 01:44:28 +0300

    Updated website.

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-const input = document.getElementById('selector');
-const preElement = document.getElementById('result');
+const input = document.getElementById("selector");
+const preElement = document.getElementById("result");
+const copyButtons = document.querySelectorAll(".copy-button");
+const copiedText = document.getElementById("copied");
 
-window.addEventListener('load', () => {
-    let text = JSON.stringify(categorizeSelector(input.value), null, 2);
-    preElement.innerText = text;
+window.addEventListener("load", () => {
+  let text = JSON.stringify(categorizeSelector(input.value), null, 2);
+  preElement.innerText = text;
 
-    input.setSelectionRange(input.value.length, input.value.length);
-    input.focus();
+  input.setSelectionRange(input.value.length, input.value.length);
+  input.focus();
+
+  copyButtons.forEach((button) => {
+    let relatedPre = button.parentElement.querySelector("pre");
+
+    button.addEventListener("click", () => {
+      let input = document.createElement("input");
+      input.value = relatedPre.innerText;
+
+      document.body.appendChild(input);
+
+      input.select();
+      input.setSelectionRange(0, 99999);
+      navigator.clipboard.writeText(input.value);
+
+      input.remove();
+    });
+  });
 });
 
-input.addEventListener('input', () => {
-    let text = JSON.stringify(categorizeSelector(input.value), null, 2);
-    preElement.innerText = text;
+input.addEventListener("input", () => {
+  let text = JSON.stringify(categorizeSelector(input.value), null, 2);
+  preElement.innerText = text;
 });
